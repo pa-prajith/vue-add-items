f3b6fb39d9c5b540dc1ced56f57c77f796c39dd8
pa-prajith <codejs100@gmail.com>
2020-10-11 01:06:45 -0700

    Vue Add Item Project

---
+++ src/components/ComponentV2.vue
@@ -0,0 +1,46 @@
+<template>
+  <h2>This is the Vue 2 style implementation</h2>
+  <div>
+      <ul v-if="items.length > 0">
+          <li v-for="(item, index) in items" :key="index">{{item.value}}</li>
+      </ul>
+      <form @submit.prevent="addItem">
+        <input type="text" v-model="newitem" placeholder="Add new item" />
+        <button type="submit">Add Item</button>
+      </form>
+  </div>
+
+</template>
+
+<script>
+export default {
+    name: 'ComponentV2',
+    data() {
+        return {
+            newitem: '',
+            items: [
+                
+            ]
+        }
+    },
+    methods: {
+        addItem() {
+            this.items.push({value: this.newitem});
+            this.newitem = '';
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+div {
+    padding: 50px;
+}
+form {
+    margin-top: 20px;
+}
+form button {
+    margin-left: 30px;
+}
+</style>
---
+++ src/components/ComponentV3.vue
@@ -0,0 +1,53 @@
+<template>
+  <h2>This is the Vue 3 style implementation</h2>
+  <div>
+      <ul v-if="state.items.length > 0">
+          <li v-for="(item, index) in state.items" :key="index">{{item.value}}</li>
+      </ul>
+      <form @submit.prevent="addItem">
+        <input type="text" v-model="state.newitem" placeholder="Add new item" />
+        <button type="submit">Add Item</button>
+      </form>
+  </div>
+
+</template>
+
+<script>
+import {reactive} from 'vue';
+
+export default {
+    name: 'ComponentV3',
+    setup() {
+        const state = reactive({
+            newitem: '',
+            items: [
+                
+            ]
+        });
+
+        const addItem = () => {
+            state.items.push({value: state.newitem});
+            state.newitem = '';
+        }
+
+        return {
+            state,
+            addItem
+        }
+    }
+
+}
+</script>
+
+<style scoped>
+
+div {
+    padding: 50px;
+}
+form {
+    margin-top: 20px;
+}
+form button {
+    margin-left: 30px;
+}
+</style>
